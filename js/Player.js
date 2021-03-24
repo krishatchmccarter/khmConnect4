fe137953b7ca1f5e8c60725d57472c2e38dc818b
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-03-24 12:43:29 -0700

    token spaces constructor methods

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,22 @@ class Player {
     this.id = id;
     this.color = color;
     this.active = active;
-    this.tokens = [];
+    this.tokens = this.createTokens(21);
+  }
+
+  /**
+   * Creates token objects for player
+   * @param {integer} num-Number of token objects to be created
+   * @return {array} tokens - an array of new token objects
+   */
+  createTokens(num) {
+    const tokens = [];
+
+    for (let i = 0; 1 < num; i++) {
+      let token = new Token(i, this);
+      tokens.push(token);
+    }
+
+    return tokens;
   }
 }
