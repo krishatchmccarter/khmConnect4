b95b5fd9eac7035b16f116785535a41b20b9faec
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-03-25 15:09:36 -0700

    startgame method

--- js/Player.js
+++ js/Player.js
@@ -9,17 +9,33 @@ class Player {
 
   /**
    * Creates token objects for player
-   * @param {integer} num-Number of token objects to be created
-   * @return {array} tokens - an array of new token objects
+   * @param   {integer}   num - Number of token objects to be created
+   * @return  {array}     tokens - an arary of new token objects
    */
   createTokens(num) {
     const tokens = [];
 
-    for (let i = 0; 1 < num; i++) {
+    for (let i = 0; i < num; i++) {
       let token = new Token(i, this);
       tokens.push(token);
     }
 
     return tokens;
+  }
+
+  /**
+   * Gets all tokens that haven't been dropped.
+   * @return {array}  Array of unused tokens.
+   */
+  get unusedTokens() {
+    return this.tokens.filter((token) => !token.dropped);
+  }
+
+  /**
+   * Gets the active token by returning the first token in the array of unused tokens.
+   * @return {Object} First token object in the array of unused tokens.
+   */
+  get activeToken() {
+    return this.unusedTokens[0];
   }
 }
